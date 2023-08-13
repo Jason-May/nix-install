cf47dd14f67cab19acf06290c5e319da3cd3502b
Jason <jwmay@ucsc.edu>
2023-08-12 23:32:57 -0700

    laptop config that is actually nice

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
-      ./users.nix
+      ./impermanence/nixos.nix
     ];
 
-  # sets nix path to persist directory
   nix.nixPath =
     [
       "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
@@ -19,121 +14,148 @@
       "/nix/var/nix/profiles/per-user/root/channels"
     ];
 
-  boot.supportedFilesystems = [ "zfs" ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # rollback on boot
+  # source: https://grahamc.com/blog/erase-your-darlings
   boot.initrd.postDeviceCommands = lib.mkAfter ''
-    zfs rollback -r rpool/root@blank
+    zfs rollback -r rpool/local/root@blank
   '';
 
+  # source: https://grahamc.com/blog/nixos-on-zfs
+  boot.kernelParams = [ "elevator=none" ];
 
   networking.hostId = "c0ffee42"; # something for zfs
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-jason"; # Define your hostname.
   networking.networkmanager.enable = true;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+  networking.dhcpcd.wait = "background";  
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  hardware.opengl.enable = true;
+
   time.timeZone = "America/Los_Angeles";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp1s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
-  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
   services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  # programs.dconf.enable = true;
+  services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
 
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  virtualisation.virtualbox.host.enable = true;
-  
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    home-manager
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    firefox
-    zfs
-    git
-    vlc
-    pavucontrol
-    unzip
-    screen
- 
-    vscode
-    discord
-    virtualbox
-    alacritty
-    kicad
-    zoom-us
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      emacs
+      zfs
+      nano
+      git
+      firefox
+      discord
+      pavucontrol
+      vlc
+      vscodium
+      qbittorrent
+      python37
+      haskell.compiler.ghc902
+      cabal-install
+      gnome.adwaita-icon-theme
+      gnomeExtensions.appindicator
+
+      minicom
+      gtkterm
+      screen
+      kitty
+
+      lutris
+      chrome-gnome-shell
+      gnome3.adwaita-icon-theme
+    ];
+  programs.steam.enable=true;
+  programs.fish.enable=true;
+
   #persist
-  environment.etc."NetworkManager/system-connections"= {
-    source = "/persist/etc/NetworkManager/system-connections";
+  fileSystems."/persist".neededForBoot = true;
+#  environment.etc."NetworkManager/system-connections"= {
+#    source = "/persist/etc/NetworkManager/system-connections";
+#  };
+#  systemd.tmpfiles.rules = [
+#    "L /var/lib/bluetooth - - - - /persist/var/lib/bluetooth"
+#  ];
+  environment.persistence."/persist" = {
+    directories = [
+      "/var/lib/bluetooth"
+      "/etc/NetworkManager/system-connections"
+    ];
   };
 
+  services.zfs = {
+    autoScrub.enable = true;
+    autoSnapshot.enable = true;
+    # TODO: autoReplication
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    hostKeys =
+      [
+        {
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/persist/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
+  };
 
-  # List services that you want to enable:
+  services.logind.lidSwitchExternalPower = "ignore"; 
+  services.logind.lidSwitch = "suspend-then-hibernate"; 
+  # ignore, suspend, suspend-then-hibernate, poweroff, lock
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  users = {
+    mutableUsers = false;
+    users = {
+      root = {
+        isSystemUser = true;
+        passwordFile = "/persist/etc/passwords/root";
+      };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
+      jason = {
+        createHome = true;
+        extraGroups = [ "wheel" "networkmanager" "audio" "dialout"];
+        group = "users";
+        uid = 1000;
+        home = "/home/jason";
+        shell=pkgs.fish;
+	      # useDefaultShell = true;
+	      isNormalUser = true;
+	      # File contains public keys for allowed SSH clients
+        openssh.authorizedKeys.keyFiles = [ /persist/etc/nixos/ssh/authorized_keys ];
+        passwordFile = "/persist/etc/passwords/jason";
+      };
+    };
+  };
+  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 
 }
-
